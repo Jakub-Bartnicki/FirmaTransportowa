b77069eb79fa68f5003afa7fede841fef9c33922
lukasz-filimoniuk-1999 <61936212+lukasz-filimoniuk-1999@users.noreply.github.com>
2020-12-04 15:49:16 +0100

    add ProductContoler for Client

---
+++ Controllers/ClientProductsController.cs
@@ -0,0 +1,36 @@
+﻿using FirmaTransportowa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirmaTransportowa.Controllers
+{
+    public class ClientProductsController : Controller
+    {
+        private DAL.TransportContext db = new DAL.TransportContext();
+        // GET: ClientProduct
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: ClientProduct/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+    }
+}
