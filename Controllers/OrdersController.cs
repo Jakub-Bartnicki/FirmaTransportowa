99f5bc70e912041a1711c22aae9c5129c03402c4
marcin97 <psinochal@gmail.com>
2020-12-18 00:34:30 +0100

    Add files via upload

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FirmaTransportowa.DAL;
+using FirmaTransportowa.Models;
+
+namespace FirmaTransportowa.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private DAL.TransportContext db = new DAL.TransportContext();
+
+        // GET: Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.Customer).Include(o => o.Payment).Include(o => o.Transport);
+            return View(orders.ToList());
+        }
+
+        // GET: Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name");
+            ViewBag.OrderID = new SelectList(db.Payments, "PaymentID", "PaymentID");
+            ViewBag.OrderID = new SelectList(db.Transports, "TransportID", "TransportID");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrderID,CustomerID,OrderDate,RequiredDate,ShippedDate,Status,Comments,Price")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", order.CustomerID);
+            ViewBag.OrderID = new SelectList(db.Payments, "PaymentID", "PaymentID", order.OrderID);
+            ViewBag.OrderID = new SelectList(db.Transports, "TransportID", "TransportID", order.OrderID);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", order.CustomerID);
+            ViewBag.OrderID = new SelectList(db.Payments, "PaymentID", "PaymentID", order.OrderID);
+            ViewBag.OrderID = new SelectList(db.Transports, "TransportID", "TransportID", order.OrderID);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrderID,CustomerID,OrderDate,RequiredDate,ShippedDate,Status,Comments,Price")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customers, "CustomerID", "Name", order.CustomerID);
+            ViewBag.OrderID = new SelectList(db.Payments, "PaymentID", "PaymentID", order.OrderID);
+            ViewBag.OrderID = new SelectList(db.Transports, "TransportID", "TransportID", order.OrderID);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
